1e569e73cb28862f4891789658d284fa1011fa06
sunnythonatos <94024600+sunnythonatos@users.noreply.github.com>
2021-11-29 21:48:23 -0500

    first commit

--- starter/backend/flaskr/__init__.py
+++ starter/backend/flaskr/__init__.py
@@ -221,11 +221,4 @@ def create_app(test_config=None):
         return (jsonify({"success": False, "error": 405,
                          "message": "method not allowed"}), 405,)
 
-    @app.errorhandler(500)
-    def server_error(error):
-        return (jsonify({
-            "success": False, "error": 500,
-            "message": "Internal Server Error"
-        }), 500)
-
     return app
